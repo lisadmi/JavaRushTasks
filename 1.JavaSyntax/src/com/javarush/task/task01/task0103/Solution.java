853a0b470e356693a842498d250634e56429bc7a
lisadmi <lisavetadmi@gmail.com>
2023-10-07 11:58:59 +0200

    add solution JavaSyntax task0103

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int year = 3126;
+        int age = 8;
+        int friend = year - age;
+        System.out.println(friend);
+    }
+}
