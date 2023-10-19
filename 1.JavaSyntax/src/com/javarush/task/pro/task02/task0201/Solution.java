222b97384040d643eae1184eea564db5371ab4e6
lisadmi <lisavetadmi@gmail.com>
2023-10-19 18:56:02 +0200

    add solution JavaSyntax task0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 6;
+        int numericValue = 9;
+        int size = 2;
+        int number = 7;
+
+    }
+}
