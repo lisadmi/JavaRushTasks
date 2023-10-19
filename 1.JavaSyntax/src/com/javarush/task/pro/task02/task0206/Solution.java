2dc899b2c894c3d383c4dea41f1bad23fe01e110
lisadmi <lisavetadmi@gmail.com>
2023-10-19 20:41:57 +0200

    add solution JavaSyntax task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       String word ="elephant";
+        String phrase ="hot dog";
+        String line ="sentence";
+        String text ="essay";
+
+    }
+}
