5f396e621a4dd0702d8251cb45c174876df3c86e
lisadmi <lisavetadmi@gmail.com>
2023-11-11 22:20:01 +0100

    add solution JavaSyntax task0311

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task03.task0311;
+
+import java.util.Scanner;
+
+/* 
+Высокая точность
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int secumber = scanner.nextInt();
+
+         if (number==secumber){
+
+        System.out.println("числа равны");}
+        else{
+}
+        System.out.println("числа не равны");}
+    }
+
