c71910b8b86279a9db54fb7da576f3c83c198223
lisadmi <lisavetadmi@gmail.com>
2023-12-26 10:06:09 +0100

    add solution JavaSyntax task0211

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0211/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0211/Solution.java
@@ -8,13 +8,9 @@ public class Solution {
 
     public static void main(String[] args) {
         String emptyString = "";
-        String bazooka = "Gomu Gomu no Bazooka!";
-        String numbers = emptyString + 2 + 2 + "22";
-        int count = emptyString.length();
-        int finish = bazooka.length();
-        int start = numbers.length();
+
         System.out.println(emptyString.length());
-        System.out.println(bazooka.length());
-        System.out.println(numbers.length());
+        System.out.println("Gomu Gomu no Bazooka!".length());
+        System.out.println((emptyString + 2 + 2 + "22").length());
     }
 }
