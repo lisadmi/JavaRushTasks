1c326e9d3e47df7e8ff6e8a4d7c11f88a3c2306b
lisadmi <lisavetadmi@gmail.com>
2024-01-03 09:51:33 +0100

    add solution JavaSyntax task0505

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task05.task0505;
+
+import java.util.Scanner;
+
+/* 
+Reverse
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+        if (number > 0) {
+            for (int i = 0; i < number; i++) {
+                int number2 = scanner.nextInt();
+                array[i]=number2;
+
+            }
+        }
+        if (number%2==0){
+            for (int i = array.length-1;i>=0;i--){
+                System.out.println(array[i]);
+            }
+        }
+        else {
+            for (int i = 0;i<array.length;i++){
+                System.out.println(array[i]);
+            }
+
+        }
+    }
+}
