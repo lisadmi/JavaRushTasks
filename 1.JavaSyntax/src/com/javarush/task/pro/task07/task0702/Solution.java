f4f7da86770046cf2770987fd8e336488fc17f5d
lisadmi <lisavetadmi@gmail.com>
2024-06-01 10:48:21 +0200

    add solution JavaSyntax task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte aByte = Byte.MAX_VALUE;
+    short aShort = Short.MAX_VALUE;
+    char aChar = Character.MAX_VALUE;
+    int aInt = Integer.MAX_VALUE;
+    long aLong = Long.MAX_VALUE;
+
+
+
+}
