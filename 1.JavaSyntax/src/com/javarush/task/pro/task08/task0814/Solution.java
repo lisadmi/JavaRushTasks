bca92c971e85d6049c5e64d0e645fe9a68befb7e
lisadmi <lisavetadmi@gmail.com>
2024-06-15 12:36:12 +0200

    add solution JavaSyntax task0814

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0814/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task08.task0814;
+
+/* 
+Флаги
+*/
+
+public class Solution {
+
+    public static int setFlag(int number, int flagPos) {
+
+        return number| (1 << flagPos);
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+
+        return number & ~ (1 << flagPos);
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(setFlag(16,2));
+        System.out.println(resetFlag(20,2));
+    }
+}
