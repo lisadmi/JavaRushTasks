fd4f7c486ba7cd0a25162ef60dc0d7639f3070b3
lisadmi <lisavetadmi@gmail.com>
2024-11-10 11:14:42 +0100

    add solution JavaSyntax task05

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0502/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0502/Solution.java
@@ -10,7 +10,16 @@ public class Solution {
     public static String[] strings = new String[5];
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 0; i < strings.length; i++) {
+            if(i % 2 == 0){
+                strings[i]=EVEN;
+
+            }
+            else{
+                strings[i]=ODD;
+            }
+
+        }
 
         System.out.print("index = 0");
         System.out.println(" value = " + strings[0]);
--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -9,18 +9,19 @@ import java.util.Arrays;
 public class Solution {
 
     public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    public static int element = 18;
+    public static int element = 5;
 
     public static void main(String[] args) {
-        int[] copy = Arrays.copyOf(array, array.length);
+        int[]copy = Arrays.copyOf(array,array.length);
         Arrays.sort(copy);
-        int index = Arrays.binarySearch(copy, element);
-        if (index >= 0) {
+        int index = Arrays.binarySearch(copy,element);
+        if ( index >= 0){
             System.out.println(true);
-
-        } else {
+        }
+        else{
             System.out.println(false);
         }
+
 
     }
 }
